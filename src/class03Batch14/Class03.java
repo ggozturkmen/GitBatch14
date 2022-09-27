d2e1b486e979cf22b38b16a3da38d76c35f05fc1
ggozturkmen <gulcangokhan02@gmail.com>
2022-09-27 18:28:33 -0500

    simple

---
+++ src/class03Batch14/Class03.java
@@ -0,0 +1,14 @@
+package class03Batch14;
+
+public class Class03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hey this is a git Class");
+		
+		
+	}
+
+}
